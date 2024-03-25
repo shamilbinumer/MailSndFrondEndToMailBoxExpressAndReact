d8d433efc3167387daad7a0dfd4086598c444a0e
shamil <shamilmohd418@gmail.com>
2024-03-25 14:41:45 +0530

    last

--- server.js
+++ server.js
@@ -1,18 +1,4 @@
-// import express from 'express';
-// import router from './router.js';
-// import cors from 'cors';
-// import dotenv from 'dotenv'
-// dotenv.config()
-// const app=express()
-// app.use(cors())
-// app.use(express.json({limit:"20mb"}));
-// app.use("/api",router);
-// app.listen(process.env.PORT,()=>{
-//     console.log("server started at ",process.env.PORT);
-// })
-
 import express from 'express';
-
 import cors from 'cors';
 import router from './router.js';
 const app = express();
